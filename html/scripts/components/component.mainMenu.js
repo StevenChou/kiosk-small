15876730cc9893265759004962423f086ad2f3c9
whalebro <steven.chou.second@gmail.com>
2020-01-21 10:51:23 +0800

    首頁設計變更

--- html/scripts/components/component.mainMenu.js
+++ html/scripts/components/component.mainMenu.js
@@ -10,18 +10,7 @@ Vue.component('component-mainMenu-main', {
         topRightCount: 0,
         keyboardLockOne: true,
         keyboardLockTwo: true
-      },
-      closingTimeList: {
-        Sunday: { closingTime: '21:30:00' },
-        Monday: { closingTime: '21:30:00' },
-        Tuesday: { closingTime: '21:30:00' },
-        Wednesday: { closingTime: '21:30:00' },
-        Thursday: { closingTime: '21:30:00' },
-        Friday: { closingTime: '22:00:00' },
-        Saturday: { closingTime: '22:00:00' }
-      },
-      openingTime: '10:50:00', // 提前十分鐘開機 10:50:00
-      closingTimer: null
+      }
     };
   },
   methods: {
@@ -46,42 +35,9 @@ Vue.component('component-mainMenu-main', {
         kiosk.API.goToNext('keyboard');
       }
     },
-    toClosingPage: function(unitTestTime) {
-      const mainMenuObj = this;
-
-      // ＊＊＊ [ 設定 timer ---> 無限迴圈 ] ＊＊＊
-      // 前十分鐘，導入到暫停營業畫面 (timer 每十秒，執行一次！！)
-      mainMenuObj.closingTimer = setInterval(function() {
-        const now = !unitTestTime ? new Date() : unitTestTime;
-        const timeFormat = 'HH:mm:ss';
-        const curDateTime = moment(now)
-          .format('YYYY-MM-DD' + ',' + 'dddd' + ',' + timeFormat)
-          .split(',');
-        const baseTimeStr =
-          mainMenuObj.closingTimeList[curDateTime[1]].closingTime;
-        const curTime = moment(curDateTime[2], timeFormat).format(timeFormat);
-        const baseTime = moment(baseTimeStr, timeFormat)
-          .subtract(10, 'minutes')
-          .format(timeFormat);
-
-        // ＊＊＊ [ 時間小於當日 10:50:00 直接導入暫停營業頁面 ] ＊＊＊
-        if (
-          moment(curDateTime[0] + ' ' + curTime).isBefore(
-            curDateTime[0] + ' ' + mainMenuObj.openingTime
-          )
-        ) {
-          kiosk.API.goToNext('closingPage');
-        }
-
-        // ＊＊＊ [ 現在時間大於關閉時間 ] ＊＊＊
-        if (
-          moment(curDateTime[0] + ' ' + curTime).isAfter(
-            curDateTime[0] + ' ' + baseTime
-          )
-        ) {
-          kiosk.API.goToNext('closingPage');
-        }
-      }, 10000);
+    settingBackgroundImage: function(imgURL) {
+      const x = document.getElementsByTagName('BODY')[0];
+      x.style.backgroundImage = imgURL;
     }
   },
   computed: {
@@ -92,14 +48,12 @@ Vue.component('component-mainMenu-main', {
   created: function() {
     kiosk.API.initStatus();
     kiosk.app.clearUserData();
-
-    // 導到暫停服務頁面！！
-    // this.toClosingPage();
-    // [UnitTest]
-    // this.toClosingPage(moment('2020-01-09 21:45:01', 'YYYY-MM-DD HH:mm:ss'));
+  },
+  mounted: function() {
+    this.settingBackgroundImage("url('img/wallpaper.png')");
   },
   beforeDestroy: function() {
-    // clearInterval(this.closingTimer);
+    this.settingBackgroundImage('none');
   }
 });
 
@@ -136,45 +90,6 @@ Vue.component('component-common-langmenu', {
             name: kiosk.wording[this.culture].mainMenu.lang04
           }
         ]
-        // [
-        //   {
-        //     next: 'remind',
-        //     culture: 'KOKR',
-        //     name: kiosk.wording[this.culture].mainMenu.lang04
-        //   },
-        //   {
-        //     next: 'remind',
-        //     culture: 'ESES',
-        //     name: kiosk.wording[this.culture].mainMenu.lang07
-        //   },
-        //   {
-        //     next: 'remind',
-        //     culture: 'THTH',
-        //     name: kiosk.wording[this.culture].mainMenu.lang05
-        //   },
-        //   {
-        //     next: 'remind',
-        //     culture: 'VIVN',
-        //     name: kiosk.wording[this.culture].mainMenu.lang10
-        //   }
-        // ]
-        // [
-        //   {
-        //     next: 'remind',
-        //     culture: '999',
-        //     name: kiosk.wording[this.culture].mainMenu.lang09
-        //   },
-        //   {
-        //     next: 'remind',
-        //     culture: '000',
-        //     name: kiosk.wording[this.culture].mainMenu.lang10
-        //   },
-        //   {
-        //     next: 'remind',
-        //     culture: '888',
-        //     name: kiosk.wording[this.culture].mainMenu.lang08
-        //   }
-        // ]
       ]
     };
   },
@@ -189,14 +104,9 @@ Vue.component('component-common-langmenu', {
       var result = this.culture == culture;
       return result;
     },
-    // Btn Click
     handleMouseDown: function(nextId) {
       kiosk.API.goToNext(nextId);
     }
   },
-  computed: {
-    // wording: function() {
-    //   return kiosk.wording[this.culture].mainMenu;
-    // }
-  }
+  computed: {}
 });
